b6f1a08f78438ba1f1f9d0344643c9f01d3ddfde
guyroberts21 <guyroberts21@outlook.com>
2021-12-04 22:14:56 +0000

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
 import React from "react"
 
+// import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/index.css"
+
+// import components
+import { Navbar, Container, Nav } from "react-bootstrap"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container fluid={true} className="section1"></Container>
+
+      <Container fluid={true} className="section2"></Container>
+
+      <Container fluid={true} className="section3"></Container>
+    </div>
+  )
 }
